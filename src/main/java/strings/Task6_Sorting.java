7696db90bfd51fb4b41821e14f824e343776bde6
IrynaGil <52880616+IrynaGil@users.noreply.github.com>
2020-05-02 19:35:49 +0300

    added some changes

--- src/main/java/strings/Task6_Sorting.java
+++ src/main/java/strings/Task6_Sorting.java
@@ -1,14 +1,10 @@
 package strings;
 
-import java.lang.reflect.Array;
-
 //Написать метод, который принимает в себя массив int*ов
 // и возвращает (return) максимальный элемент.
 public class Task6_Sorting {
 
     public static void main(String[] args) {
-        
-
         System.out.println(price(1));
     }
 
@@ -19,13 +15,9 @@ public class Task6_Sorting {
 
         int res = 0;
 
-
+        //TODO write code here
 
         return res;
     }
-
-
-
-
 
 }
