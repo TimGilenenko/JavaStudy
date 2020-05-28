04abe3c417ca498e2bb1d12fe14290971617117d
IrynaGil <52880616+IrynaGil@users.noreply.github.com>
2020-05-28 20:14:37 +0300

    task 6

--- src/main/java/strings/Task6_Sorting.java
+++ src/main/java/strings/Task6_Sorting.java
@@ -1,21 +1,33 @@
 package strings;
 
+import java.util.Arrays;
+
 //Написать метод, который принимает в себя массив int*ов
 // и возвращает (return) максимальный элемент.
 public class Task6_Sorting {
 
+    private static int[] numbers = {1, 2, 3, 5, 4};
+
+
     public static void main(String[] args) {
-        System.out.println(price(1));
+        System.out.println(Arrays.toString(numbers));
+
+        System.out.println(findNumber(numbers));
     }
 
-    public static Integer price(Integer intr) {
-        if (intr == null) {
-            return null;
+
+    public static Integer findNumber(int[] numbers) {
+        if (numbers.length == 0) {
+            return 0;
         }
 
         int res = 0;
 
-        //TODO write code here
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            if (res < numbers[i]) {
+                res = numbers[i];
+            }
+        }
 
         return res;
     }
